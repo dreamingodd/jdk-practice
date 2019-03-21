3e6bed435d835595a5d6850f8d4d6bcb22f693bd
YWD@MVC <memorywilllast@gmail.com>
2019-03-21 16:07:16 +0800

    Add personalized of method in collection factory method demo.

--- src/main/java/xyz/trendfashion/practice/CollectionFactoryDemo.java
+++ src/main/java/xyz/trendfashion/practice/CollectionFactoryDemo.java
@@ -1,6 +1,8 @@
 package xyz.trendfashion.practice;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class CollectionFactoryDemo {
     public static void main(String[] args) {
@@ -8,12 +10,22 @@ public class CollectionFactoryDemo {
         stringSet.forEach(System.out::print);
         System.out.println("-");
 
-        Set<Integer> integerSet = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        Integer sum = integerSet.stream().reduce(Integer::sum).get();
+        Set<Integer> integerSet1 = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Set<Integer> integerSet2 = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Integer sum = integerSet1.stream().reduce(Integer::sum).get();
         System.out.println(sum);
         // parallel
-        sum = integerSet.stream().parallel().reduce(Integer::sum).get();
+        sum = integerSet2.stream().parallel().reduce(Integer::sum).get();
         System.out.println(sum);
+    }
+
+    private static <T> Set<T> of (T... values) {
+        Set<T> set = new LinkedHashSet<>();
+        // Add
+        Stream.of(values).forEach(value ->{
+            set.add(value);
+        });
+        return set;
     }
 
 
