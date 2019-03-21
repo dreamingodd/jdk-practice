e018e8da6d173fdc42f5cd4d883949cdb5cac8a8
YWD@MVC <memorywilllast@gmail.com>
2019-03-21 18:24:54 +0800

    Most stream methods covered.

--- src/main/java/xyz/trendfashion/practice/eight/LambdaDemo.java
+++ src/main/java/xyz/trendfashion/practice/eight/LambdaDemo.java
@@ -2,18 +2,26 @@ package xyz.trendfashion.practice.eight;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LambdaDemo {
+
     private static List<String> stringList = Arrays.asList("Kobe", "Michael", "Michelle", "Huntington", "Peter", "Johnson", "Clinton");
+    private static List<Integer> integerList = Arrays.asList(-10, 11, 432, 1, 129129, -772, 1, -10);
+
     public static void main(String[] args) {
         sortOriginal();
         sortLambda();
         toLowerCaseLamda();
         addSuffix();
+        findMax();
+        filterNegative();
+        distinct();
+        flatMap();
+        sum();
     }
 
     private static void sortOriginal() {
-        List<Integer> integerList = Arrays.asList(-10, 11, 432, 1, 129129);
         Collections.sort(integerList, new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -35,7 +43,47 @@ public class LambdaDemo {
     }
 
     private static void addSuffix() {
-        List<String> suffixedStringList = stringList.stream().map(s -> {return s += "suffix";}).collect(Collectors.toList());
+        List<String> suffixedStringList = stringList.stream().map(s -> s += "suffix").collect(Collectors.toList());
         System.out.println(suffixedStringList);
+    }
+
+    private static void findMax() {
+        Integer max = integerList.stream().reduce(Integer::max).get();
+        System.out.println(max);
+    }
+
+    private static void filterNegative() {
+        List<Integer> negativeList = integerList.stream().filter(i -> i < 0).collect(Collectors.toList());
+        System.out.println(negativeList);
+    }
+
+    private static void distinct() {
+        List<Integer> distinctList = integerList.stream().distinct().collect(Collectors.toList());
+        System.out.println(distinctList);
+    }
+
+    // others such as: peek, limit, skip.
+    private static void flatMap() {
+        Stream<List<Integer>> stream1 = Stream.of(
+                Arrays.asList(1, 9),
+                Arrays.asList(2, 3),
+                Arrays.asList(4, 5, 6)
+        );
+        Stream<Integer> stream2 = stream1.flatMap((childList) -> childList.stream());
+        stream2.forEach(o -> {
+            System.out.print(o);
+            System.out.print(" ");
+        });
+        System.out.println();
+    }
+
+    // others such as: allMatch, anyMatch, findFirst, noneMatch, max, min.
+    private static void sum() {
+        List<Integer> integerList1 = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            integerList1.add(i + 1);
+        }
+        Integer sum = integerList1.stream().reduce(-50, (tmpSum, item) -> tmpSum + item);
+        System.out.println(sum);
     }
 }
